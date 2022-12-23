995ef0b1f486fc17176d2730020e8f79b4c56328
Hà Thị Ngọc Lan <121292061+hangoclan92@users.noreply.github.com>
2022-12-23 18:29:51 +0700

    Add files via upload

---
+++ CourseManProg.java
@@ -0,0 +1,18 @@
+package a2_1901040115;
+
+import a2_1901040115.View.MainWindow;
+
+public class CourseManProg {
+
+
+    public static void main(String[] args)  {
+
+        MainWindow w = new MainWindow();
+        w.setVisible(true);
+
+
+    }
+
+
+
+}
